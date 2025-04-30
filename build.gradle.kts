b5e0075031bd5a9946cb18dea56df73162a9c7d6
Stream <1542763342@qq.com>
2025-05-01 01:10:58 +0800

    build: use version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    kotlin("jvm") version "2.1.20"
-    id("org.graalvm.buildtools.native") version "0.10.6"
-    id("com.gradleup.shadow") version "8.3.5"
-    kotlin("plugin.serialization") version "2.1.20"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.graalvm.buildtools)
+    alias(libs.plugins.gradleup.shadow)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 group = "io.github.stream29"
@@ -14,28 +14,28 @@ repositories {
 
 dependencies {
     // Ktor server
-    implementation("io.ktor:ktor-server-core:3.1.2")
-    implementation("io.ktor:ktor-server-cio:3.1.2")
-    implementation("io.ktor:ktor-server-call-logging:3.1.2")
-    implementation("io.ktor:ktor-server-content-negotiation:3.1.2")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-rx2:1.10.2")
-
-    implementation("com.alibaba:dashscope-sdk-java:2.19.4") {
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.cio)
+    implementation(libs.ktor.server.call.logging)
+    implementation(libs.ktor.server.content.negotiation)
+    
+    implementation(libs.kotlinx.coroutines.rx2)
+    
+    implementation(libs.dashscope.sdk.java) {
         exclude("org.slf4j", "slf4j-simple")
     }
-
-    implementation("io.ktor:ktor-serialization-kotlinx-json:3.1.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
-
-    implementation("ch.qos.logback:logback-classic:1.4.11")
-
-    implementation("io.github.stream29:streamlin:3.1.0")
-    implementation("io.github.stream29:json-schema-generator:1.0.2")
-    implementation("com.charleskorn.kaml:kaml:0.77.1")
-
+    
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.kotlinx.serialization.json)
+    
+    implementation(libs.logback.classic)
+    
+    implementation(libs.streamlin)
+    implementation(libs.json.schema.generator)
+    implementation(libs.kaml)
+    
     testImplementation(kotlin("test"))
-    testImplementation("io.ktor:ktor-server-test-host:3.1.2")
+    testImplementation(libs.ktor.server.test.host)
 }
 
 kotlin {
