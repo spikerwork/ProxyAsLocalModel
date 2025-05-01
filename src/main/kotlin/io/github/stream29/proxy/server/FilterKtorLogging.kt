c29a88bd92f42f2e2fb95d5cf825895e671c6e60
Stream <1542763342@qq.com>
2025-05-02 02:07:29 +0800

    refactor: better filter ktor messages

--- src/main/kotlin/io/github/stream29/proxy/server/FilterKtorLogging.kt
+++ src/main/kotlin/io/github/stream29/proxy/server/FilterKtorLogging.kt
@@ -5,8 +5,13 @@ import org.slf4j.Logger
 fun Logger.filterKtorLogging(): Logger =
     object : Logger by this {
         override fun info(msg: String) {
-            if (msg.startsWith("Application started in") || msg.startsWith("Responding at"))
-                return
+            if (ktorLoggingPrefixes.any { msg.startsWith(it) }) return
             this@filterKtorLogging.info(msg)
         }
     }
+
+private val ktorLoggingPrefixes = listOf(
+    "Application started in",
+    "Responding at",
+    "Autoreload is disabled because the development mode is off."
+)
