7763e8001c22a81c559955e7f5de54f8825cc873
Stream <1542763342@qq.com>
2025-05-02 08:37:38 +0800

    feat: better error handling

--- src/main/kotlin/io/github/stream29/proxy/Main.kt
+++ src/main/kotlin/io/github/stream29/proxy/Main.kt
@@ -3,8 +3,13 @@ package io.github.stream29.proxy
 import kotlinx.coroutines.delay
 
 suspend fun main() {
-    // Just to initialize Global.kt
-    config
+    try {
+        // Just to initialize Global.kt
+        config
+    } catch (t: Throwable) {
+        t.printStackTrace()
+        println("Failed to init proxy: ${t.message}")
+    }
     while (true) {
         delay(10000)
     }
